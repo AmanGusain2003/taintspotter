ce88bc1577b2a19360626b0d31381c88761323b3
AmanGusainChitkra <aman4814.be21@chitkara.edu.in>
2022-12-16 05:11:35 +0530

    This has a resolved button working

--- app_functions.py
+++ app_functions.py
@@ -10,16 +10,10 @@ from PyQt5.QtWidgets import (
     QHBoxLayout
 )
 
-def make_buttons(Layout1, setting, centralWidget):
-    
-    butt_add_sys = QPushButton("Add new System", centralWidget)
-    butt_remove_sys = QPushButton("Remove a System",centralWidget)
-    butt_sys_list = QPushButton("System List", centralWidget)
-    butt_setting = QPushButton("Setting", centralWidget)
-    butt_add_sys.setGeometry(100, 30, 150, 25)
-    butt_remove_sys.setGeometry(100, 60, 150, 25)
-    butt_sys_list.setGeometry(100, 90, 150, 25)
-    butt_setting.setGeometry(10, 30, 46, 20)
-    # Layout1.addWidget(butt_add_sys)
-    # Layout1.addWidget(butt_remove_sys)
-    # Layout1.addWidget(butt_sys_list)
+def add_device(name, gui_win):
+    gui_win.device_list.addItem(name)
+
+def add_complaints(gui_win):
+    complaint = "Complaint No. " + str(gui_win.complaints_list.count() + 1)
+    gui_win.complaints_list.addItem(complaint)
+
--- taint_spotter.py
+++ taint_spotter.py
@@ -6,6 +6,7 @@ from functools import partial
 from settings import Setting
 from main_window import main_guiWindow
 from welcome_gui import welcome_screen
+import app_functions as app_functions
 from PyQt5.QtCore import Qt
 from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import (
@@ -26,13 +27,20 @@ main_Widget = QStackedWidget()
 
 gui_win = main_guiWindow(setting)
 welcome_gui = welcome_screen()
-gui_win.make_buttons()
+# gui_win.make_buttons()
 # gui_win.show()
 main_Widget.addWidget(welcome_gui)
 main_Widget.addWidget(gui_win)
 main_Widget.setGeometry(50, 50, 800, 600)
 def flip_screen():
     main_Widget.setCurrentIndex(1)
+    app_functions.add_device("Square 1", gui_win)
+    app_functions.add_device("Edison", gui_win)
+    app_functions.add_device("IBN Boys Hostel", gui_win)
+    app_functions.add_complaints(gui_win)
+    app_functions.add_complaints(gui_win)
+    timer.stop()
+
 main_Widget.show()
 timer = QTimer()
 timer.start(2000)
